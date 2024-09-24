23bb8e0c01f4005b295ece172cb7615a3f283616
Danny Mok <dmok50@stuy.edu>
2024-09-23 20:00:44 -0400

    finished

--- 09_softserve/v0/app.py
+++ 09_softserve/v0/app.py
@@ -3,12 +3,12 @@
 # September 2024
 
 from flask import Flask
-app = Flask(__name__)          # ...
+app = Flask(__name__)          # This probably determines the name of the app
 
-@app.route("/")                # ...
+@app.route("/")                # This "routes" the app to /?
 def hello_world():
-    print(__name__)            # ...
-    return "No hablo queso!"   # ...
+    print(__name__)            # Prints the "__name__" in the terminal
+    return "No hablo queso!"   # Returns this text on the website
 
-app.run()                      # ...
+app.run()                      # Runs the actual app
                 
