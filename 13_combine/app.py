2d5e715edba39b1750788868537180f4d7f58dfb
Danny <dmok50@stuy.edu>
2024-10-07 10:53:32 -0400

    push

--- 13_combine/app.py
+++ 13_combine/app.py
@@ -1,24 +1,34 @@
+
 # Danny Mok
 # Cerulean Calm: Danny, Alex, Colyi
 # SoftDev
 # Sep 2024
 
 from flask import Flask, render_template
+import csv
+import random
 app = Flask(__name__)
 
-f = open("data/occupations.csv", "r").read()
-f = f.split("\n")[1:-1] # Turns data into list
     
-flist = []
-    
-for i in f:
-    i = i.split(",")
-    i = ",".join(i).replace('"',"")
-    flist.append(i)
+occupations = [] #list and dict for occupations
+
+with open("data/occupations.csv", "r") as file:
+    f = csv.reader(file)
+    next(f) # skips first row     
+    for line in f:
+        if len(line) == 3:
+            job, percentage, link = line
+            job = job.replace('"', '') #removes extra quotes
+            occupations.append({
+                'job': job,
+                'percentage' : float(percentage),
+                'link': link
+            })
 
 @app.route("/")
 def fxn():
-    return render_template( 'tablified.html', foo="fooooo", collection=flist)
+    random_occupation = random.choice(occupations)
+    return render_template( 'tablified.html', random_occupation = random_occupation, occupations = occupations)
 
 if __name__ == "__main__":
     app.debug = True
