f4367f399410bdc2631f6099ab72f8bb365c7dbd
Danny <dmok50@stuy.edu>
2024-10-08 10:30:37 -0400

    added teamates' work

--- 14_intake/app.py
+++ 14_intake/app.py
@@ -1,6 +1,6 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# October 2024
+# Danny Mok
+# Ghidorah: Mark, Marco, Danny
+# 10-07-2024
 
 # import conventions:
 # list most general first (standard python library)
@@ -11,7 +11,7 @@ from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
 
-import testmod0
+import testmod0 #imports all methods from testmod0.py
 
 #the conventional way:
 #from flask import Flask, render_template, request
@@ -39,24 +39,19 @@ PROTIP: Insert your own in-line comments
    understand what is going on.
 '''
 
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
-    #print("\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app)
-        #Return <Flask 'app'>
-    #print("***DIAG: request obj ***")
-    #print(request)
-        #Returns <Request 'http://localhost:5000/' [GET]>
-    #print("***DIAG: request.args ***")
-    #print(request.args)
-        #Returns an empty dictionary called ImmutableMultiDict
-    #print("***DIAG: request.args['username']  ***") 
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
-        #Errors, there is no key named username in the ImmutableMultiDict
-    #print("***DIAG: request.headers ***")
-    #print(request.headers)
-        #Gives header info of the website
+    print("***DIAG: request.headers ***")
+    print(request.headers)
     return render_template( 'login.html' )
 
 
@@ -65,21 +60,14 @@ def authenticate():
     #print("\n\n\n")
     #print("***DIAG: this Flask obj ***")
     #print(app)
-        #Return <Flask 'app'>
     #print("***DIAG: request obj ***")
     #print(request)
-        #Returns 127.0.0.1 - - [07/Oct/2024 23:43:48] "GET /auth?username=wahoo!&sub1=Submit HTTP/1.1" 200 -
     #print("***DIAG: request.args ***")
     #print(request.args)
-        #Returns a dictionary called ImmutableMultiDict, this time with the button text input and the submit button input
-        #ImmutableMultiDict([('username', 'wahoo!'), ('sub1', 'Submit')])
     #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
-        #Returns the value of the 'username' key
-        #wahoo!
     #print("***DIAG: request.headers ***")
     #print(request.headers)
-        #Gives header info of the website
     return "Waaaa hooo HAAAH"  #response to a form submission
 
 
